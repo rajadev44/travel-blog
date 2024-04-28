23848206a76f1031fb82eb6b967c239517751aa6
alesya <bondarea2@roehampton.ac.uk>
2024-04-28 23:30:12 +0100

    user.js update

--- app/model/user.js
+++ app/model/user.js
@@ -46,6 +46,24 @@ class User {
         return true;    
     }
 
+    // Test a submitted password against a stored password
+    async authenticate(submitted) {
+        var sql = "SELECT password FROM Users WHERE id = ?";
+        const result = await db.query(sql, [this.id]);
+        const match = await bcrypt.compare(submitted, result[0].password);
+        if (match == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    
+
+
+}
+
 module.exports  = {
     User
 }
